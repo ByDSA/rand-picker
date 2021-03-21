584076b416ca1e19ae21bbf695e3e2aadb1ee12e
Daniel Sales Álvarez <dsa21may@gmail.com>
2021-03-21 01:32:16 +0100

    Export PickerOptions and PickOptions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { create } from "./CreatePicker";
 import { Picker } from "./Picker";
+import { PickerOptions } from "./PickerOptions";
+import { PickOptions } from "./PickOptions";
 
-export { create as newPicker, Picker };
+export { create as newPicker, Picker, PickerOptions, PickOptions };
